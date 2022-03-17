f4e89dbf73d5221754ba8de2485e955dccf644ab
cke0devel <cke0devel@gmail.com>
2022-03-17 11:17:40 +0900

    [leetcode/2154] solved Keep Multiplying Found Values by Two

---
+++ leetcode/solved/2154_Keep_Multiplying_Found_Values_by_Two.py
@@ -0,0 +1,3 @@
+# difficulty: Easy
+# Runtime: 99 ms, faster than 54.43% of Python3 online submissions for Keep Multiplying Found Values by Two.
+# Memory Usage: 14.1 MB, less than 68.44% of Python3 online submissions for Keep Multiplying Found Values by Two.
