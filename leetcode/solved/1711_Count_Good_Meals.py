ce4c6421a9b1cc2eedad9a226f8ad5b279bc9dde
cke0devel <cke0devel@gmail.com>
2022-03-18 00:20:15 +0900

    [leetcode/1711] solved Count Good Meals

--- leetcode/solved/1711_Count_Good_Meals.py
+++ leetcode/solved/1711_Count_Good_Meals.py
@@ -1,11 +1,12 @@
 # difficulty: Medium
-# Wrong Answer
+# Runtime: 1432 ms, faster than 64.88% of Python3 online submissions for Count Good Meals.
+# Memory Usage: 21.7 MB, less than 51.79% of Python3 online submissions for Count Good Meals.
 
 class Solution:
     def countPairs(self, deliciousness: List[int]) -> int:
         from collections import Counter
 
-        power2 = [2**i for i in range(20) if 2**i<2*10**5]
+        power2 = [2**i for i in range(22)]
 
         deliciousness.sort()
         c = Counter(deliciousness)
@@ -21,5 +22,5 @@ class Solution:
                 if target in c:
                     ans += c[target]
 
-        return ans
+        return ans % (10**9 + 7)
 
