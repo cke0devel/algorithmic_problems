8c193d897b927920437bcbbc85abfe66b4bed1dd
cke0devel <cke0devel@gmail.com>
2022-03-23 08:30:26 +0900

    [leetcode/263] solved Ugly Number

--- leetcode/solved/263_Ugly_Number.py
+++ leetcode/solved/263_Ugly_Number.py
@@ -1,9 +1,12 @@
 # difficulty: Easy
-# Time Limit Exceeded
-#   0
+# Runtime: 54 ms, faster than 33.33% of Python3 online submissions for Ugly Number.
+# Memory Usage: 13.9 MB, less than 24.32% of Python3 online submissions for Ugly Number.
 
 class Solution:
     def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
         while n%2 == 0:
             n = n // 2
 
