b65ba3f8c5a61844a700a0fad89ea60b04ffabaf
cke0devel <cke0devel@gmail.com>
2022-03-24 23:45:15 +0900

    [leetcode/781] Wrong Answer Rabbits in Forest

--- leetcode/781_Rabbits_in_Forest.py
+++ leetcode/781_Rabbits_in_Forest.py
@@ -1,10 +1,10 @@
 # difficulty: Medium
 # Wrong Answer
-#   Input: [1,0,1,0,0]
-#   Output: 3
-#   Expected: 5
+#   Input: [[0,0,1,1,1]]
+#   Output: 4
+#   Expected: 6
 
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        return sum(n+1 for n in set(answers))
+        return sum(n+1 for n in set(answers) if n>0) + answers.count(0)
 
