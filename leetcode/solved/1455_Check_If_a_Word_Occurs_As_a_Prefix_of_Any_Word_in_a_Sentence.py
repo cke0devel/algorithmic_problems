156aee623cace0446a9328574e80b4a73d77b180
cke0devel <cke0devel@gmail.com>
2022-05-06 23:55:05 +0900

    [leetcode/1455] solved Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ leetcode/solved/1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.py
@@ -0,0 +1,14 @@
+# difficulty: Easy
+# Runtime: 50 ms, faster than 26.30% of Python3 online submissions for Check If a Word Occurs As a Prefix of Any Word in a Sentence.
+# Memory Usage: 13.8 MB, less than 97.71% of Python3 online submissions for Check If a Word Occurs As a Prefix of Any Word in a Sentence.
+
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        words = sentence.split()
+
+        for i, word in enumerate(words):
+            if word.startswith(searchWord):
+                return i + 1
+
+        return -1
+
