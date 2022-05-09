2956a0410d85dd9d9f27a97f0be10bc92cc72ef1
cke0devel <cke0devel@gmail.com>
2022-05-09 09:41:07 +0900

    [leetcode/344] solved Reverse String

---
+++ leetcode/solved/344_Reverse_String.py
@@ -0,0 +1,11 @@
+# difficulty: Easy
+# Runtime: 195 ms, faster than 96.18% of Python3 online submissions for Reverse String.
+# Memory Usage: 18.4 MB, less than 88.77% of Python3 online submissions for Reverse String.
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+
