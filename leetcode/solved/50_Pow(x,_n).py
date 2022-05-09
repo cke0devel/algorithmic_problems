cc1c01af80462839456d0cf6902c167a888520de
cke0devel <cke0devel@gmail.com>
2022-05-09 17:30:49 +0900

    [leetcode/50] solved Pow(x, n)

---
+++ leetcode/solved/50_Pow(x,_n).py
@@ -0,0 +1,8 @@
+# difficulty: Medium
+# Runtime: 32 ms, faster than 85.32% of Python3 online submissions for Pow(x, n).
+# Memory Usage: 13.7 MB, less than 99.85% of Python3 online submissions for Pow(x, n).
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return pow(x,n)
+
