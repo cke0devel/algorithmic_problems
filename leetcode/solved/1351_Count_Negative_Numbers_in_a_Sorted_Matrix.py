9019eaf18871ff4e83a640c900d407237624ccd0
cke0devel <cke0devel@gmail.com>
2022-05-11 16:42:39 +0900

    [leetcode/1351] solved Count Negative Numbers in a Sorted Matrix

---
+++ leetcode/solved/1351_Count_Negative_Numbers_in_a_Sorted_Matrix.py
@@ -0,0 +1,3 @@
+# difficulty:
+# Runtime: 232 ms, faster than 11.08% of Python3 online submissions for Count Negative Numbers in a Sorted Matrix.
+# Memory Usage: 15 MB, less than 38.05% of Python3 online submissions for Count Negative Numbers in a Sorted Matrix.
